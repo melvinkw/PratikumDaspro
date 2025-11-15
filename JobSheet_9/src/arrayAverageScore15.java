83a6738dd32e03da6903195ae9c7aaef036ab0d7
Melvink <melvinkwiryawan01@gmail.com>
2025-11-15 17:56:42 +0700

    nambah file

--- JobSheet_9/src/arrayAverageScore15.java
+++ JobSheet_9/src/arrayAverageScore15.java
@@ -4,33 +4,37 @@ public class arrayAverageScore15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] score = new int[10];
-        double total = 0;
-        double average;
-        int passed = 0;
+        int numberStudent = 0;
+        double totalPassed = 0;
+        double totalnotpassed = 0;
+        double averagepassed, averagenotpassed;
+        int passed = 0, notpassed = 0;
 
+        System.out.print( "Enter the number of students: ");
+        numberStudent = sc.nextInt();
+
+        int[] score = new int[numberStudent];
         for (int i = 0; i < score.length; i++) {
             System.out.print("Enter score " + (i + 1) + ": ");
             score[i] = sc.nextInt();
         }
 
         for (int i = 0; i < score.length; i++) {
-            total += score[i];
             if (score[i] > 70) {
                 passed++;
+                totalPassed += score[i];
+            } else {
+                notpassed++; 
+                totalnotpassed += score [i];
             }
         }
 
-        average = total / score.length;
+        averagepassed = totalPassed / passed;
+        averagenotpassed = totalnotpassed / notpassed;
 
-        System.out.println("\n=== Score List ===");
-        for (int i = 0; i < score.length; i++) {
-            System.out.println("Student " + (i + 1) + ": " + score[i]);
-        }
+        System.out.println("The average score of students who passed is: " + averagepassed);
+        System.out.println("The average score of students who not passed is: " + averagenotpassed);
 
-        System.out.println("\nTotal Score : " + total);
-        System.out.println("Average     : " + average);
-        System.out.println("Passed      : " + passed + " students");
 
         sc.close();
     }
